b4db5250e7a1fd30b570c48b329f957ea535e977
Diyarbek Oralbaev <diyarbekoralbaev@gmail.com>
2024-05-24 23:44:53 +0500

    some models for foot intake

--- qr_code/admin.py
+++ qr_code/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import QRCodeModel, FoodIntakeRecord
 
-admin.site.register(QRCodeModel)
-admin.site.register(FoodIntakeRecord)
+
+class QRCodeModelAdmin(admin.ModelAdmin):
+    list_display = ('user', 'code', 'created_at', 'updated_at', 'expired_at', 'is_expired')
+    list_filter = ('user', 'created_at', 'updated_at', 'expired_at')
+    search_fields = ('user', 'code', 'created_at', 'updated_at', 'expired_at')
+    readonly_fields = ('created_at', 'updated_at', 'expired_at', 'is_expired')
+
+
+class FoodIntakeRecordAdmin(admin.ModelAdmin):
+    list_display = ('user', 'taken_at')
+    list_filter = ('user', 'taken_at')
+    search_fields = ('user', 'taken_at')
+    readonly_fields = ('taken_at')

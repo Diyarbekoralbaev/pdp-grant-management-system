7c463975bd4272c80f06508aa436c720212ddfe8
Diyarbek Oralbaev <diyarbekoralbaev@gmail.com>
2024-05-24 23:18:20 +0500

    models added to admin panel

--- qr_code/admin.py
+++ qr_code/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .views import QRCodeView, QRCodeDetailView
 
-# Register your models here.
+admin.site.site_header = "QR Code Admin"
+admin.site.site_title = "QR Code Admin Portal"
+admin.site.index_title = "Welcome to QR Code Portal"
+admin.site.register(QRCodeView)
+admin.site.register(QRCodeDetailView)
